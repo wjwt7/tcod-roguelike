0a6b7f362899aa4626fa98fad557cb94ed785136
wjwt7 <wilsontruong7@outlook.com>
2026-09-07 23:01:25 -0400

    fix: correct event_handler initialization in Engine class

--- engine.py
+++ engine.py
@@ -13,7 +13,7 @@ class Engine:
     game_map: GameMap
 
     def __init__(self, player: Entity):
-        self.event_handler = EventHandler = EventHandler(self)
+        self.event_handler: EventHandler = EventHandler(self)
         self.player = player
 
     def handle_enemy_turns(self) -> None:
