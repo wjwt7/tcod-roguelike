6b032e0ded48e22c709e992411c3218ac9638bb7
wjwt7 <wilsontruong7@outlook.com>
2026-09-07 22:52:15 -0400

    add actors property and get_actor_at_location method to GameMap class

--- game_map.py
+++ game_map.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 import numpy as np # type: ignore
 from tcod.console import Console
 
+from entity import Actor
 import tile_types
 
 if TYPE_CHECKING:
@@ -18,6 +19,15 @@ class GameMap:
         self.visible = np.full((width, height), fill_value=False, order="F") # Tiles the player can currently see
         self.explored = np.full((width, height), fill_value=False, order="F") # Tiles the player has seen before
 
+    @property
+    def actors(self) -> Iterator[Actor]:
+        """Iterate over this map's living actors."""
+        yield from (
+            entity
+            for entity in self.entities
+            if isinstance(entity, Actor) and entity.is_alive
+        )
+
     def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
         for entity in self.entities:
             if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
@@ -25,6 +35,12 @@ class GameMap:
 
         return None
 
+    def get_actor_at_location(self, x: int, y: int) -> Optional[Actor]:
+        for actor in self.actors:
+            if actor.x == x and actor.y == y:
+                return actor
+        return None
+    
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
         return 0 <= x < self.width and 0 <= y < self.height
